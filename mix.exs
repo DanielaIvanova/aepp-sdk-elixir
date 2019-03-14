b44790f20a7683836cf7932b10ea6b99a3930b6c
danielaivanova <danielaivanova_@abv.bg>
2019-03-14 11:03:12 +0200

    Introducing Elixir SDK

--- mix.exs
+++ mix.exs
@@ -1,29 +1,38 @@
-defmodule AeppSdkElixir.MixProject do
+defmodule AeternityNode.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :aepp_sdk_elixir,
+      app: :aeternity_node,
       version: "0.1.0",
       elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {AeppSDKElixir, []}
-    ]
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 4.0"},
-      {:httpoison, "~> 1.5"}
+      {:tesla, "~> 1.2.1"},
+      {:poison, ">= 4.0.1"}
     ]
   end
 end
